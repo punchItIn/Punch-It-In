3723e698127aba5d0d70e0874112155ab2d8dabd
Rampel Isaiah Orofino <riorofino@dynamicobjx.com>
2012-04-28 00:56:25 -0400

    added punch-android

---
+++ punch-android/src/main/java/com/punch/PunchActivity.java
@@ -0,0 +1,13 @@
+package com.punch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PunchActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle s) {
+		super.onCreate(s);
+		setContentView(R.layout.main);
+	}
+}
